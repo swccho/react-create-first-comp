6d603cbb0ada9940bcf5e2e9c654247395d8ec7f
Ali Haider Swccho <swccho263@gmail.com>
2023-12-19 17:32:09 +0600

    component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import KgButton from "./KgButton.jsx";
+import Hello from "./Hello.jsx";
+import Random from "./Random.jsx";
+
 function App() {
-    return <h1>
-        Hello JSX
-    </h1>
+    return <div>
+        <h1>Hello JSX</h1>
+        <Hello />
+        <Random />
+    </div>
+
 }
 export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello(){
+    let myName = 'AHS'
+    let fullName = () => {
+        return 'Ali Haider Swccho'
+    }
+
+    return <h3>
+        Hello this is the future speaking. I am your savior {fullName()}
+    </h3>
+}
+export default Hello
